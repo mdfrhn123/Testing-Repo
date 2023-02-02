c86fca95a7a25deba08089aaa5500231f3a35bf5
Mohammad Farhan <mdfrhn123@gmail.com>
2023-02-02 19:54:46 +0530

    My Last commit

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("Newton School Learning");
 console.log("good");
 console.log("third commit for understanding github more");
 console.log("I am this adding manually");
+console.log("Now I Understood github");
