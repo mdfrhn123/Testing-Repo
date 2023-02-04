efaf96f8be6885b723364f1e4ef8080425d4bd37
Mohammad Farhan <mdfrhn123@gmail.com>
2023-02-04 10:56:02 +0530

    dom Use

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ console.log("good");
 console.log("third commit for understanding github more");
 console.log("I am this adding manually");
 console.log("Now I Understood github");
+const l1 = document.getElementById("ls1");
+l1.innerHTML = "changed";
+let cl = document.getElementsByClassName("cl");
+for(let i=0;i<cl.length;i++){
+    cl[i].innerHTML = "ye le";
+}
+
+
