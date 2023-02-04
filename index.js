30ee518d6c262b61055ad430a743f0938b7b02a2
Mohammad Farhan <mdfrhn123@gmail.com>
2023-02-05 00:43:20 +0530

    changed

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-console.log("Newton School Learning");
-console.log("good");
-console.log("third commit for understanding github more");
-console.log("I am this adding manually");
-console.log("Now I Understood github");
-const l1 = document.getElementById("ls1");
-l1.innerHTML = "changed";
-let cl = document.getElementsByClassName("cl");
-for(let i=0;i<cl.length;i++){
-    cl[i].innerHTML = "ye le";
+let p = document.getElementById("anchor");
+p.addEventListener('click',prevent);
+function prevent(e){
+    e.preventDefault();
+    console.log("Link Clicked");
 }
-
-
